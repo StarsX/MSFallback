a02ad7898e455a5e78125ed7144726718377a6c8
StarsX <X-Universe@outlook.com>
2020-11-19 10:59:42 +0800

    Reduce duplicated meshlet index fetch.

--- MSFallback/Content/Shaders/CSMeshletMS.hlsl
+++ MSFallback/Content/Shaders/CSMeshletMS.hlsl
@@ -16,11 +16,10 @@ StructuredBuffer<uint> DispatchMeshArgs : register (t5);
 	moc.PrimCount = primCount; \
 }
 
-#define GET_MESHLET_IDX(i) DispatchMeshArgs[i]
+#define GET_MESHLET_IDX(i) meshletIdx = DispatchMeshArgs[i]
 #define OUT_IDX(i) 0
-#define indices
+#define indices uint meshletIdx, out
 #define vertices MeshOutCounts moc, out
-#define indices
 
 #define main MSMain
 #include "MSMeshlet.hlsl"
@@ -36,20 +35,18 @@ void main(uint dtid : SV_DispatchThreadID, uint gtid : SV_GroupThreadID, uint gi
 	uint3 tris[MAX_PRIM_COUNT];
 	Payload payload = (Payload)0;
 	MeshOutCounts moc = (MeshOutCounts)0;
-	MSMain(dtid, gtid, gid, payload, moc, verts, tris);
-
-	// Load the meshlet from the AS payload data
-	const uint meshletIndex = DispatchMeshArgs[gid];
+	uint meshletIdx;
+	MSMain(dtid, gtid, gid, payload, moc, verts, meshletIdx, tris);
 
 	if (gtid < MAX_PRIM_COUNT)
 	{
-		const uint baseAddr = 3 * (MAX_PRIM_COUNT * meshletIndex + gtid);
-		const uint baseIdx = MAX_VERT_COUNT * meshletIndex;
+		const uint baseAddr = 3 * (MAX_PRIM_COUNT * meshletIdx + gtid);
+		const uint baseIdx = MAX_VERT_COUNT * meshletIdx;
 
 		[unroll]
 		for (uint i = 0; i < 3; ++i)
 			IndexPayloads[baseAddr + i] = gtid < moc.PrimCount ? baseIdx + tris[0][i] : 0xffffffff;
 	}
 
-	if (gtid < moc.VertCount) VertexPayloads[MAX_VERT_COUNT * meshletIndex + gtid] = verts[0];
+	if (gtid < moc.VertCount) VertexPayloads[MAX_VERT_COUNT * meshletIdx + gtid] = verts[0];
 }
